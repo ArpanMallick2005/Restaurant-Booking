b607bfab6265dc5a71a3724d13a04288dec832bb
Arpan Mallick <mallickarpan53@gmail.com>
2026-02-07 23:34:00 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-booking-o3wf.onrender.com",
+        "https://restaurant-booking-o3wf.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
